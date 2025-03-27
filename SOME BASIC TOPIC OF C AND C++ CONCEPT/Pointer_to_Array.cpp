fca05342962a2718c7883f4f9dfaa876ce7b4167
Pankajyadavup53 <py0354895@gmail.com>
2025-03-27 21:58:45 +0530

    Some modification in program

--- SOME BASIC TOPIC OF C AND C++ CONCEPT/Pointer_to_Array.cpp
+++ SOME BASIC TOPIC OF C AND C++ CONCEPT/Pointer_to_Array.cpp
@@ -8,7 +8,7 @@ int main() {
     int arr[5] = {2, 4, 6, 8, 10}; // Declare and initialize an array
     int len = sizeof(arr) / sizeof(arr[0]); // Calculate array length
 
-    int *p = arr; // Corrected: Set pointer to the first element of the array
+    int *p = arr; // Set pointer to the first element of the array
 
     // Loop to print the address of each element
     for (int i = 0; i < len; i++) {
